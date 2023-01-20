c8029168e6bf1b3cd5de3c00b86113aa422d22e1
SakT1122 <sak.978tirbhane@gmail.com>
2023-01-20 20:06:26 +0530

    Revised ans

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,26 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Main {
+    public static class Product{
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
+
     public static void main(String[] args) {
-        Product p=new Product();
-        p.product(9,12);
-        p.product(7,8,9);
-        p.product(7.1,5.6);
+        Product p = new Product();
+        System.out.println(p.product(10,10));
+        System.out.println(p.product(25,2,2));
+        System.out.println(p.product(9.95, 88.85));
+
     }
 }
 
-class Product{
-    public int product(int x, int y){
-        return x*y;
-    }
-    public int product(int x, int y, int z){
-        return x*y*z;
-    }
-    public double product(double x, double y){
-        return x*y;
-    }
-}
